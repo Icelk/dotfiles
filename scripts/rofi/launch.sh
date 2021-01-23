de1b5a36dee7582f35e7b9f5e8c4544e51d9207d
Icelk <main@icelk.dev>
2021-01-23 18:40:40 +0100

    Cleaned rofi script

--- scripts/rofi/launch.sh
+++ scripts/rofi/launch.sh
@@ -8,5 +8,4 @@ elif [ "$menu" = "appmenu" ]; then
     rofi -show drun -theme clean
 elif [ "$menu" = "powermenu" ]; then
     rofi -modi 'Powermenu:~/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu
-#    rofi -modi 'Powermenu:~/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu -location 3 -xoffset -24 -yoffset 70
 fi
