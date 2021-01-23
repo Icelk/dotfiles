43350f7e41004d9b9256a7672e6e537b91716046
Icelk <main@icelk.dev>
2021-01-23 22:26:55 +0100

    Added notifications to screen shot tool

--- scripts/screenshot-whole.sh
+++ scripts/screenshot-whole.sh
@@ -2,3 +2,5 @@
 path=~/Pictures/screenshot.png
 
 maim $path
+
+notify-send -a "Maim" "Took screenshot of whole screen and saved it to '$path'!"
--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,3 +2,5 @@
 path=~/Pictures/screenshot.png
 
 maim -su $path
+
+notify-send -a "Maim" "Took screenshot and saved it to '$path'!"
