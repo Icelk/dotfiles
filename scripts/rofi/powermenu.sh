2a650c5e07427ca3cb113bae62b1892f71b7f4fb
Icelk <main@icelk.dev>
2021-02-01 19:47:23 +0100

    Changed powermenu option name

--- scripts/rofi/powermenu.sh
+++ scripts/rofi/powermenu.sh
@@ -2,13 +2,13 @@
 
 if [ -z "$@" ]; then
     echo -en "Shutdown\0icon\x1fsystem-shutdown-symbolic\n"
-    echo -en "Relog\0icon\x1fsystem-log-out-symbolic\n"
+    echo -en "Close GUI\0icon\x1fsystem-log-out-symbolic\n"
     echo -en "Suspend\0icon\x1fsystem-suspend-symbolic\n"
     echo -en "Reboot\0icon\x1fsystem-restart-symbolic\n"
 else
     if [ "$1" = "Shutdown" ]; then
         systemctl poweroff
-    elif [ "$1" = "Relog" ]; then
+    elif [ "$1" = "Close GUI" ]; then
         i3-msg exit
     elif [ "$1" = "Reboot" ]; then
         systemctl reboot
