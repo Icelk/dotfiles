ba891cfbc7629d977097e8af8b09d4e58aa5c908
Icelk <main@icelk.dev>
2021-02-08 18:37:10 +0100

    Fixed reminders script and units

--- scripts/send-reminders.sh
+++ scripts/send-reminders.sh
@@ -1,13 +1,19 @@
 #!/bin/fish
 
+set init_time 120
+set interval_time 10
+
 set application "Reminder"
 
 set file ~/Reminder.txt
 set content (cat $file)
 
+sleep $init_time
+
 for line in $content
     if test -n "$line"
         notify-send -a $application $line
+        sleep $interval_time
     end
 end
 
