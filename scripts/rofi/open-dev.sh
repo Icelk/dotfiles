46b5ff5ad8a04986e7ad37b4a17a8a0b7b053375
Icelk <main@icelk.dev>
2021-02-09 07:59:10 +0100

    Fixed bug when entering wrong names in open dev script

--- scripts/rofi/open-dev.sh
+++ scripts/rofi/open-dev.sh
@@ -2,13 +2,18 @@
 # Copyright © Icelk 2021-present
 
 dev_path=~/dev/
+editor="${EDITOR:-codium}"
 
 language=$(ls $dev_path | rofi -i -dmenu -p "Choose language")
+if [ $? -ne 0 ]; then exit 1; fi
 
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+new_list=$(ls $dev_path"$language")
+if [ $? -ne 0 ]; then exit 1; fi
 
 project=$(ls $dev_path"$language" | rofi -i -dmenu -p "Choose project")
 
-codium $dev_path"$language"/"$project"
+ls $dev_path"$language"/"$project" >/dev/null
+if [ $? -ne 0 ]; then exit 1; fi
+
+$editor $dev_path"$language"/"$project"
+
