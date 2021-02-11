ec9441b99bb40da931ce1b1504a02eee8ba15e88
Icelk <main@icelk.dev>
2021-02-11 20:50:45 +0100

    Ran corpl and changed dunst colors

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -2,5 +2,5 @@
 
 theme=$1
 
-corpl ~/.config/polybar/polybar.ini -c ";" -e $theme
-corpl ~/.config/i3/config ~/.config/dunst/dunstrc -e $theme
+corpl ~/.config/polybar/config.ini -c ";" -e $theme
+corpl ~/.config/i3/config ~/.config/dunst/dunstrc -c "#" -e $theme
