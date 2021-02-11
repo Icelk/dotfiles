b42bdea3a2dd5429b305f83a99deb515a78492e4
Icelk <main@icelk.dev>
2021-02-11 21:02:24 +0100

    Added alacritty to theme changer

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -3,5 +3,5 @@
 theme=$1
 
 corpl ~/.config/polybar/config.ini -c ";" -e $theme
-corpl ~/.config/i3/config ~/.config/dunst/dunstrc -e $theme
+corpl ~/.config/i3/config ~/.config/dunst/dunstrc ~/.config/alacritty/alacritty.yml -e $theme
 i3-msg restart
