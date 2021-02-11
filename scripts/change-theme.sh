62b5e4c4fcc67459df5037456238ea22cfcc9212
Icelk <main@icelk.dev>
2021-02-11 21:00:00 +0100

    Fixed comment recognition in dunstrc

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -3,5 +3,5 @@
 theme=$1
 
 corpl ~/.config/polybar/config.ini -c ";" -e $theme
-corpl ~/.config/i3/config ~/.config/dunst/dunstrc -c "#" -e $theme
+corpl ~/.config/i3/config ~/.config/dunst/dunstrc -e $theme
 i3-msg restart
