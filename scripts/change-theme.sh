5a3f90aa2be6bbf89f3c6818361cc4a04b276837
Icelk <main@icelk.dev>
2021-02-13 11:11:49 +0100

    Added CORPL to rofi

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -4,4 +4,5 @@ theme=$1
 
 corpl ~/.config/polybar/config.ini -c ";" -e $theme
 corpl ~/.config/i3/config ~/.config/dunst/dunstrc ~/.config/alacritty/alacritty.yml -e $theme
+corpl ~/.config/rofi/colors.rasi --comment "/*" --closing-comment "*/" -e $theme
 i3-msg restart
