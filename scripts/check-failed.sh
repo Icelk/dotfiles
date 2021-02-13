4f1a23fcfbc7b284273ed70068510f206dfa803a
Icelk <main@icelk.dev>
2021-02-13 12:29:38 +0100

    Renamed scripts and changed check failed script notification

--- scripts/check-failed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/fish
-
-set failed (math (echo (echo (systemctl list-units --failed | tail -n 1) | string split ' ')[1]) + (echo (echo (systemctl --user list-units --failed | tail -n 1) | string split ' ')[1]))
-
-if test $failed -eq 1
-    notify-send -a "Service Failed" -u critical "$failed service has failed. See systemctl status for more info."
-else if test $failed -ne 0
-   notify-send -a "Service Failed" -u critical "$failed services have failed. See systemctl status for more info."
-end
---
+++ scripts/systemctl-check-failed.sh
@@ -0,0 +1,9 @@
+#!/bin/fish
+
+set failed (math (echo (echo (systemctl list-units --failed | tail -n 1) | string split ' ')[1]) + (echo (echo (systemctl --user list-units --failed | tail -n 1) | string split ' ')[1]))
+
+if test $failed -eq 1
+    notify-send -a "Service Failed" -u critical "$failed service has failed. See 'systemctl list-units --failed' for more info."
+else if test $failed -ne 0
+    notify-send -a "Service Failed" -u critical "$failed services have failed. See 'systemctl list-units --failed' for more info."
+end
