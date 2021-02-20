c747c5dd3a095922109f742a56269adee32b58a6
Icelk <main@icelk.dev>
2021-02-20 09:07:53 +0100

    Now does not sync (and therefore delete files, --del flag) mountpoints if no fs is mounted.

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -4,20 +4,34 @@ echo "Starting backup sync!"
 
 # -P: progress h: human a: archive v: verbose
 
-rsync -a --del --delete-excluded /home icelk@nas:/mnt/main/computer_erik/ --exclude-from="/home/icelk/Exclude-backup.txt"
+rsync -ahP --del --delete-excluded /home icelk@nas:/mnt/main/computer_erik/ --exclude-from="/home/icelk/Exclude-backup.txt"
 echo "Done with /home"
-rsync -a --del --delete-excluded /var icelk@nas:/mnt/main/computer_erik/ --exclude-from="/home/icelk/Exclude-backup.txt"
+rsync -ahP --del --delete-excluded /var icelk@nas:/mnt/main/computer_erik/ --exclude-from="/home/icelk/Exclude-backup.txt"
 echo "Done with /var"
-rsync -a --del --delete-excluded /usr/share icelk@nas:/mnt/main/computer_erik/usr/ --exclude-from="/home/icelk/Exclude-backup-usr-share.txt"
+rsync -ahP --del --delete-excluded /usr/share icelk@nas:/mnt/main/computer_erik/usr/ --exclude-from="/home/icelk/Exclude-backup-usr-share.txt"
 echo "Done with /usr/share"
-rsync -a --del --delete-excluded /usr/local icelk@nas:/mnt/main/computer_erik/usr/
+rsync -ahP --del --delete-excluded /usr/local icelk@nas:/mnt/main/computer_erik/usr/
 echo "Done with /usr/local"
-rsync -a --del --delete-excluded /etc icelk@nas:/mnt/main/computer_erik/ --exclude-from="/home/icelk/Exclude-backup.txt"
+rsync -ahP --del --delete-excluded /etc icelk@nas:/mnt/main/computer_erik/ --exclude-from="/home/icelk/Exclude-backup.txt"
 echo "Done with /etc"
-rsync -a --del --delete-excluded /boot icelk@nas:/mnt/main/computer_erik/ --exclude-from="/home/icelk/Exclude-backup.txt"
-echo "Done with /boot"
 
-rsync -a --del --delete-excluded /archive icelk@nas:/mnt/main/computer_erik/ --exclude-from="/home/icelk/Exclude-backup-archive.txt"
-echo "Done with /archive"
-rsync -a --del --delete-excluded /win icelk@nas:/mnt/main/computer_erik/ --exclude-from="/home/icelk/Exclude-backup-win.txt"
-echo "Done with /win"
+mounted=$(mount -l)
+
+if echo $mounted | grep -q "/boot "; then
+    rsync -ahP --del --delete-excluded /boot icelk@nas:/mnt/main/computer_erik/ --exclude-from="/home/icelk/Exclude-backup.txt"
+    echo "Done with /boot"
+else
+    echo "/boot is not mounted, not syncing."
+fi
+if echo $mounted | grep -q "/archive "; then
+    rsync -ahP --del --delete-excluded /archive icelk@nas:/mnt/main/computer_erik/ --exclude-from="/home/icelk/Exclude-backup-archive.txt"
+    echo "Done with /archive"
+else
+    echo "/archive is not mounted, not syncing."
+fi
+if echo $mounted | grep -q "/win "; then
+    rsync -ahP --del --delete-excluded /win icelk@nas:/mnt/main/computer_erik/ --exclude-from="/home/icelk/Exclude-backup-win.txt"
+    echo "Done with /win"
+else
+    echo "/win is not mounted, not syncing."
+fi
