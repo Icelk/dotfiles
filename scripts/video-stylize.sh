42defd3f29fffa0b659441061c361f5645e759d7
Icelk <main@icelk.dev>
2021-03-03 09:35:10 +0100

    Added styalize script for v4l2 video

---
+++ scripts/video-stylize.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+
+b=100
+c=5
+s=83
+
+case $1 in
+    old)
+        b=75
+        c=10
+        s=0
+        ;;
+    *)
+esac
+
+v4l2-ctl --set-ctrl brightness=$b
+v4l2-ctl --set-ctrl contrast=$c
+v4l2-ctl --set-ctrl saturation=$s
