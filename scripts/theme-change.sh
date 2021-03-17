c7f219a5a1b642c130ab7b4dd650de2702ef7ff3
Icelk <main@icelk.dev>
2021-03-17 14:19:07 +0100

    Added dunst to theme change script

--- scripts/theme-change.sh
+++ scripts/theme-change.sh
@@ -6,6 +6,7 @@ hc=~/.config
 corpl $hc/picom/picom.conf $hc/i3/config $hc/dunst/dunstrc $hc/alacritty/alacritty.yml ~/scripts/rofi/powermenu.sh -e $theme
 corpl $hc/rofi/colors.rasi --comment "/*" --closing-comment "*/" -e $theme
 i3-msg restart
+systemctl --user restart dunst
 
 # Do poly bar later, so i3 had time to restart
 corpl $hc/polybar/config.ini -c ";" -e $theme
