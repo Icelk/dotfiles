9d6cfca719e26fe5187a29499da936716b86bfaf
Icelk <main@icelk.dev>
2021-03-18 10:28:55 +0100

    Audio record script now has a `--list` flag built in.

--- scripts/audio-record.sh
+++ scripts/audio-record.sh
@@ -6,11 +6,15 @@ output=$2
 if [ "$1" == "--help" ]; then
     echo -e "\
 First argument is source. \
-Use \`pactl list short sources\` to get available sources. \
+Use the flag \`--list\` to get available sources. \
 Alternatively, use \`default\` for your default input source.\n\
 \n\
 Second argument is output file. This script will append \`.m4a\` to it."
     exit 1
+fi
+if [ "$1" == "--list" ]; then
+    pactl list short sources
+    exit
 fi
 
 ffmpeg -f pulse -i $device -ac 2 $output.m4a
