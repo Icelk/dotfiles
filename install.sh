f80bd0d5c3d2701411db299a7a05706bde15839a
Icelk <main@icelk.dev>
2021-03-21 14:23:28 +0100

    Removed imwheel. It caused several scroll issues.

--- install.sh
+++ install.sh
@@ -94,7 +94,6 @@ l $wdh/zprofile ~/.zprofile
 l $wdh/zshrc ~/.zshrc
 l $wdh/Config-files.txt ~/
 l $wdh/Reminder.txt ~/
-l $wdh/imwheelrc ~/.imwheelrc
 
 ## Backup exclude
 l $wdh/Exclude-backup.txt ~/
