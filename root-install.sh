a26ebcabb794759467d278084f4c18ec3ec590ec
Icelk <main@icelk.dev>
2021-05-11 11:19:51 +0200

    NeoVim is symlinked to vi and vim on root-install.

--- root-install.sh
+++ root-install.sh
@@ -8,3 +8,8 @@ chown root:wheel $spotifyd
 
 launchctl load -w $spotifyd
 launchctl start $spotifyd
+
+## Symlink vi and vim to NeoVim
+bin="/usr/local/bin"
+ln -s $bin/nvim $bin/vim
+ln -s $bin/nvim $bin/vi
