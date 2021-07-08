b62a053ba91eef4b433ce23a5e2b8ef1602b3ac9
Icelk <main@icelk.dev>
2021-07-08 11:05:25 +0200

    Several scripts now use 'notify-send' to signal initiation and competion.

--- scripts/xmodmap-toggle.sh
+++ scripts/xmodmap-toggle.sh
@@ -2,6 +2,8 @@
 
 if xmodmap -pke | grep -q "keycode *67 *= *bracket"; then
     xmodmap ~/.Xmodmap-alternative
+    notify-send -a "Xmodmap switcher" "Now using alternative layout."
 else
     xmodmap ~/.Xmodmap
+    notify-send -a "Xmodmap switcher" "Now using default layout."
 fi
