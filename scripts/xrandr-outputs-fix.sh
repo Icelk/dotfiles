61c1cb53f8b7aacbd563520ae1bb9ffcf049af05
Icelk <main@icelk.dev>
2021-08-02 14:38:00 +0200

    Adjusted xrandr fix script. Still not working 100% though.

--- scripts/xrandr-outputs-fix.sh
+++ scripts/xrandr-outputs-fix.sh
@@ -6,11 +6,8 @@ rate=165
 xrandr --output DP-2 --mode $mode --rate $rate
 xrandr --output DP-0 --mode $mode --rate $rate
 
-xrandr --outout DP-2 --panning 2560x1440+0+0
-xrandr --outout DP-0 --primary --panning 2560x1440+0+0
-xrandr --outout DP-2 --pos 0x0
-xrandr --outout DP-0 --right-of DP-2 --primary
-
+xrandr --outout DP-2 --panning 2560x1440+0+0 --left-of DP-0
+xrandr --outout DP-0 --primary --panning 2560x1440+0+0 --right-of DP-2
 
 sleep 5
 systemctl --user restart polybar nitrogen
