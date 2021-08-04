29eee2c084ae1c7ee1f8721fdf34453f0d999ad8
Icelk <main@icelk.dev>
2021-08-04 09:55:58 +0200

    Hopefully fixed packages-upgradable-count script.

--- scripts/packages-upgradable-count.sh
+++ scripts/packages-upgradable-count.sh
@@ -4,17 +4,17 @@
 while ! ip address | grep "state UP">/dev/null; do sleep 0.25; done
 
 # The filename of your apps pid file.
-PIDFILE="packages-upgradable-count.pid"
+# PIDFILE="packages-upgradable-count.pid"
 
 # Wait while it exists
-while [[ -f "/tmp/run/$PIDFILE" ]]; do sleep 0.5; done
+# while [[ -f "/tmp/run/$PIDFILE" ]]; do sleep 0.5; done
 # Make sure the directory exists
-mkdir -p "/tmp/run/"
+# mkdir -p "/tmp/run/"
 # Create file
-echo $$ > "/tmp/run/$PIDFILE"
+# echo $$ > "/tmp/run/$PIDFILE"
 
 OUTPUT="ERROR"
-while [[ "$OUTPUT" == *"ERROR"* ]]; do OUTPUT=$(timeout 5 checkupdates); done
+while [[ "$OUTPUT" == *"ERROR"* ]]; do sleep 0.2; OUTPUT=$(timeout 5 checkupdates 2>&1); done
 if [[ -z $OUTPUT ]]; then
 	echo 0
 else
@@ -22,4 +22,4 @@ else
 fi
 
 # Delete it
-rm -f "/tmp/run/$PIDFILE"
+# rm -f "/tmp/run/$PIDFILE"
