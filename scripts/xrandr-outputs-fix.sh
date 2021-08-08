cc1f347690cd2d3c95445e93e3c50abb230e82f3
Icelk <main@icelk.dev>
2021-08-08 21:09:26 +0200

    Hopefully fixed xrandr-outputs-fix script.

--- scripts/xrandr-outputs-fix.sh
+++ scripts/xrandr-outputs-fix.sh
@@ -3,11 +3,11 @@
 mode="2560x1440"
 rate=165
 
-xrandr --output DP-2 --mode $mode --rate $rate
 xrandr --output DP-0 --mode $mode --rate $rate
+xrandr --output DP-2 --mode $mode --rate $rate
 
-xrandr --outout DP-2 --panning 2560x1440+0+0 --left-of DP-0
-xrandr --outout DP-0 --primary --panning 2560x1440+0+0 --right-of DP-2
+xrandr --output DP-0 --panning 2560x1440+0+0 --primary --pos 0x0
+xrandr --output DP-2 --panning 2560x1440+0+0 --left-of DP-0
 
 sleep 5
 systemctl --user restart polybar nitrogen
