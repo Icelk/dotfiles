8aabb47c2a8a88e160dc6915cf9ae03c63db2cc4
Icelk <main@icelk.dev>
2021-08-25 11:16:59 +0200

    Checking for faulty services now ignores failed mounts.

--- scripts/systemctl-check-failed.fish
+++ scripts/systemctl-check-failed.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 
-set failed (math (echo (echo (systemctl list-units --failed | tail -n 1) | string split ' ')[1]) + (echo (echo (systemctl --user list-units --failed | tail -n 1) | string split ' ')[1]))
+set failed (math (echo (echo (systemctl list-units --failed --type service | tail -n 1) | string split ' ')[1]) + (echo (echo (systemctl --user list-units --failed | tail -n 1) | string split ' ')[1]))
 
 if test $failed -eq 1
     notify-send -a "Service Failed" -u critical "$failed service has failed. See 'systemctl list-units --failed' for more info."
