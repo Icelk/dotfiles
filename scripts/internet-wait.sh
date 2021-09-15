b2442c8d365db352b02ab8d90dcaaf74f9a99ddb
Icelk <main@icelk.dev>
2021-09-15 20:31:30 +0200

    Made internet-wait.sh more reliable.

--- scripts/internet-wait.sh
+++ scripts/internet-wait.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 # Wait for an ip address
-while ! ip address | grep "192.168.">/dev/null; do sleep 1; done
+while ! ip address | grep "scope global dynamic">/dev/null; do sleep 1; done
 
 # Do work!
