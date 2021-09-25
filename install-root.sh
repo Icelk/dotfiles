bfcba9696cf251334bb898c5ca437956ea03676c
Icelk <main@icelk.dev>
2021-09-25 13:51:20 +0200

    Cleaned up install-root.sh.

--- install-root.sh
+++ install-root.sh
@@ -56,54 +56,61 @@ l $wdr/dpms.conf /etc/X11/xorg.conf.d/40-dpms.conf
 # Pacman config
 l $wdr/pacman.conf /etc/
 
-# Enable auto-login
-#mkdir /etc/systemd/system/getty@tty1.service.d
-#ln -s $PWD/root/getty.conf /etc/systemd/system/getty@tty1.service.d/override.conf
-echo
-echo "An automatic login configuration file is included but not in this install script due to the username being needed. The line above (in this script) can be used to create a symlink."
-
-l $wdr/Xwrapper.config /etc/X11/
-#ln -s $PWD/root/xorg.conf /etc/X11/
-echo "A xorg configuration file is included but not in this install script due to hardware-specific configuration. The line above (in this script) can be used to create a symlink."
-
-#ln -s $PWD/root/fstab /etc/
-#ln -sf $PWD/root/btrbk.conf /etc/btrbk/
-echo "btrbk (backup utility) and fstab config files in included but commented out in this install script due to hardware-specific mount-points."
-echo
-
-# rustup pacman hook
-l $wdr/rustup.hook /etc/pacman.d/hooks/
-echo "A rustup pacman hook has been installed. It will update Rust when the system updates. To remove it, unlink /etc/pacman.d/hooks/rustup.hook"
-echo
-# Kora icon theme hook
-l $wdr/kora.hook /etc/pacman.d/hooks/
-l $wdr/breeze.hook /etc/pacman.d/hooks/
-l $wdr/nvidia.hook /etc/pacman.d/hooks/
-
 echo "Linking vi and vim to nvim, and sudo to doas."
 
 l $bin/nvim $bin/vi
 l $bin/nvim $bin/vim
 l $bin/doas $bin/sudo
 
+# rustup pacman hook
+l $wdr/rustup.hook /etc/pacman.d/hooks/
+echo "A rustup pacman hook has been installed. It will update Rust when the system updates. To remove it, unlink /etc/pacman.d/hooks/rustup.hook"
+
+# Kora icon theme hook
+l $wdr/kora.hook /etc/pacman.d/hooks/
+# Breeze icon theme hook
+l $wdr/breeze.hook /etc/pacman.d/hooks/
+l $wdr/nvidia.hook /etc/pacman.d/hooks/
+
+l $wdr/Xwrapper.config /etc/X11/
+
+
+# Enable auto-login
+#mkdir /etc/systemd/system/getty@tty1.service.d
+#ln -s $PWD/root/getty.conf /etc/systemd/system/getty@tty1.service.d/override.conf
+
+#ln -s $PWD/root/xorg.conf /etc/X11/
+
+#ln -s $PWD/root/fstab /etc/
+#ln -sf $PWD/root/btrbk.conf /etc/btrbk/
+
+echo
+echo "An automatic login configuration file is included but not in this install script due to the username being needed. The line above (in this script) can be used to create a symlink."
+echo "A xorg configuration file is included but not in this install script due to hardware-specific configuration. The line above (in this script) can be used to create a symlink."
+echo "btrbk (backup utility) and fstab config files in included but commented out in this install script due to hardware-specific mount-points."
+
+
 echo
 read -p "The following will override certain system files. Press Ctrl+C to quit."
 read -p "Please make sure this is what you want; it'll DELETE the target files!"
 read -p "Also make sure your user is part of the 'wheel' group as the doas configuration file only allows users in that group."
+
+# avahi
+l -f $wdr/nsswitch.conf /etc/
 
 # Reduced pam failure timeout
 l -f $wdr/pam-system-auth /etc/pam.d/system-auth
 
 # Hosts
 l -f $wdr/hosts /etc/
-echo "The hosts file contains 'icelk' as the name of the computer. You might want to change that if you want another name to link to localhost."
+echo "The hosts file contains 'icelk' as the name of this computer. You might want to change that if you want another name to link to localhost."
 
 # makepkg
 l -f $wdr/makepkg.conf /etc/
 
 # doas
 cp $wdr/doas.conf /etc/
-chown root /etc/doas.conf
+chown root:root /etc/doas.conf
 
 # pacman
 l -f $wdr/pacman.conf /etc/
@@ -111,9 +118,6 @@ l -f $wdr/pacman.conf /etc/
 # reflector
 # ln -sf $wdr/reflector.conf /etc/xdg/reflector/
 echo "A reflector configuration file was included, but it contains location-specific options and isn't installed. See this script for the command."
-
-# avahi
-l -f $wdr/nsswitch.conf /etc/
 
 echo
 read -p "Services will now be started. The rest of the installation is successful. Press Ctrl+C to quit."
