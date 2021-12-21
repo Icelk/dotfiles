1a988236f65951757f39d7b77beb946047892e1f
Icelk <main@icelk.dev>
2021-12-21 23:35:19 +0100

    Decreased slowflake count.

--- scripts/xsnow.sh
+++ scripts/xsnow.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-export DISPLAY=:0
-xsnow -snowflakes 400 -notrees -nokeepsnowonscreen -whirl 2 -wsnowdepth 2 -delay 30 -windtimer 60
+xsnow -snowflakes 200 -notrees -nokeepsnowonscreen -whirl 2 -wsnowdepth 2 -delay 30 -windtimer 60
