ad04f980a344a633ef1a1dc04d5078c9dec1e6a9
Icelk <main@icelk.dev>
2022-04-14 11:50:01 +0200

    Added Firefox settings through user.js.

---
+++ config/firefox-user.js
@@ -0,0 +1,36 @@
+// enable overscroll like on macOS. Makes flinging (on touchpad) feel nicer
+user_pref("apz.overscroll.enabled", true);
+// decrease friction after flinging - makes a fling travel longer on the page
+user_pref("apz.fling_friction", 0.05);
+
+// Enable the shorter tabbar.
+user_pref("browser.compactmode.show", true);
+
+// Hide some horrendous search engines
+user_pref("browser.search.hiddenOneOffs", "Google,Amazon.se,Bing");
+
+// Enable HTTPS-only mode
+user_pref("dom.security.https_only_mode", true);
+user_pref("dom.security.https_only_mode_ever_enabled", true);
+
+// Enable WebGPU.
+user_pref("dom.webgpu.enabled", true);
+
+// Remove full screen warning
+user_pref("full-screen-api.warning.timeout", 0);
+
+// Make scroll wheel scrolling feel more snappy
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+
+// Performance
+user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.compositor", true);
+user_pref("gfx.webrender.svg-images", true);
+user_pref("layers.acceleration.force", true);
+
+// it says it itself
+user_pref("privacy.resistFingerprinting", true);
+user_pref("privacy.resistFingerprinting.testGranularityMask", 2);
+user_pref("privacy.spoof_english", 1);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.socialtracking.enabled", true);
--- install.sh
+++ install.sh
@@ -58,6 +58,10 @@ l $wdc/cargo.toml ~/.cargo/config.toml
 l $wdc/clipcat/ $hc/clipcat
 mkdir -p ~/.cache/clipcat/clipcatd
 
+## Firefox
+
+l $wdc/firefox-user.js ~/.mozilla/firefox/$(/bin/ls -1 ~/.mozilla/firefox/ | rg --pcre2 "^(?!static-).*\\.default-release" | head -n1)/user.js
+
 ## Fish
 l $wdc/fish/ $hc/fish
 
