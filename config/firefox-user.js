665a08818dc8df02efe2b9387153b10d42fc80cf
Icelk <main@icelk.dev>
2022-06-29 12:31:31 +0200

    Disabled firefox resistFingerprinting, as it caused issues with timing in my code.

--- config/firefox-user.js
+++ config/firefox-user.js
@@ -29,7 +29,7 @@ user_pref("gfx.webrender.svg-images", true);
 user_pref("layers.acceleration.force", true);
 
 // it says it itself
-user_pref("privacy.resistFingerprinting", true);
+user_pref("privacy.resistFingerprinting", false);
 user_pref("privacy.resistFingerprinting.testGranularityMask", 4);
 user_pref("privacy.spoof_english", 1);
 user_pref("privacy.trackingprotection.enabled", true);
