53c468fc512e1022a13fed23bdff038dd1e5334c
Icelk <main@icelk.dev>
2022-11-03 14:57:25 +0100

    Move to using wayland.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
 if status --is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx
+        exec Hyprland
+        # exec startx
     end
 end
 
--- scripts/packages-ratio.sh
+++ scripts/packages-ratio.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 all=$(pacman -Q | grep "" -c)
-echo "$all"
+# echo "$all"
 
 upgradable=$(. ~/scripts/packages-upgradable-count.sh)
 if [ $upgradable == 0 ]; then
