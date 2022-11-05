f620451b902f1c61eef0a77547c3b00b96987dac
Icelk <main@icelk.dev>
2022-11-05 11:43:47 +0100

    Don't use NVIDIA settings for hyprland.

--- scripts/wayland.sh
+++ scripts/wayland.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export LIBVA_DRIVER_NAME=nvidia
-export XDG_SESSION_TYPE=wayland
-export GBM_BACKEND=nvidia-drm
-export __GLX_VENDOR_LIBRARY_NAME=nvidia
-export WLR_NO_HARDWARE_CURSORS=1
+# export LIBVA_DRIVER_NAME=nvidia
+# export XDG_SESSION_TYPE=wayland
+# export GBM_BACKEND=nvidia-drm
+# export __GLX_VENDOR_LIBRARY_NAME=nvidia
+# export WLR_NO_HARDWARE_CURSORS=1
 Hyprland 2>&1 > hyprland-$(date +%Y-%d-%m_%H:%M:%S).log
