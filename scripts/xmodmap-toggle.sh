1b6316d940adb94b233c82eaf9e6f3b02159d839
Icelk <main@icelk.dev>
2022-11-11 06:53:12 +0100

    Remove xmodmap switcher script.

--- scripts/xmodmap-toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if xmodmap -pke | grep -q "keycode *67 *= *bracket"; then
-    xmodmap ~/.Xmodmap-alternative
-    notify-send -a "Xmodmap switcher" "Now using alternative layout."
-else
-    xmodmap ~/.Xmodmap
-    notify-send -a "Xmodmap switcher" "Now using default layout."
-fi
