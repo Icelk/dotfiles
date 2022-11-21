f86ca4ae1d7e153b6bb6da29955e6d91a0f0121d
Icelk <main@icelk.dev>
2022-11-21 11:38:16 +0100

    Fix theme change script & powermenu GUI close.

--- scripts/theme-change.sh
+++ scripts/theme-change.sh
@@ -12,11 +12,7 @@ for theme in $themes; do
     flags+="-e $theme "
 done
 
-corpl $hc/picom/picom.conf $hc/i3/config $hc/dunst/dunstrc $hc/kitty/kitty.conf $hc/hypr/hyprland.conf $flags
+corpl $hc/dunst/dunstrc $hc/kitty/kitty.conf $hc/hypr/hyprland.conf $flags
 corpl $hc/rofi/colors.rasi $hc/waybar/style.css --comment "/*" --closing-comment "*/" $flags
-i3-msg restart
 killall -s SIGUSR1 kitty
 systemctl --user restart dunst waybar
-
-# Do polybar later, so i3 had time to restart
-corpl $hc/polybar/config.ini -c ";" $flags
