abd9554e432723e3bab25091a82a5ad3e81d7943
Icelk <main@icelk.dev>
2022-11-30 17:58:34 +0100

    Fixed kb-toggle script. It now finds the corpl binary.

--- scripts/kb-toggle.sh
+++ scripts/kb-toggle.sh
@@ -2,6 +2,8 @@
 
 alternate=$(rg -q "# kb_variant = ansi-switched-slash" ~/.config/hypr/hyprland.conf; echo $?)
 
+PATH=~/.cargo/bin:$PATH
+
 if [ $alternate == "0" ]; then
     corpl -k -e ansi-switched ~/.config/hypr/hyprland.conf
     notify-send -a "Keyboard layout" "Now using alternate layout."
