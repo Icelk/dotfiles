f44c6aca6979ddda4f55d9b729bda6c393febee3
Icelk <main@icelk.dev>
2022-12-04 22:53:38 +0100

    Neovim: Disable some null_ls sources.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -284,10 +284,9 @@ lspc.html.setup {
 local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
-        null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.completion.spell,
         null_ls.builtins.diagnostics.fish,
-        null_ls.builtins.diagnostics.php,
+        -- null_ls.builtins.diagnostics.php,
         null_ls.builtins.formatting.fish_indent,
         null_ls.builtins.hover.dictionary,
     },
