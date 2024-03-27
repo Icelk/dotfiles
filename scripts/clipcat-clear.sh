f6e53d9e6aadcdebd8719cc8d199044b71c7c126
Icelk <main@icelk.dev>
2024-03-27 12:52:46 +0100

    Make clipboard manager great!

--- scripts/clipcat-clear.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-PATH=~/.cargo/bin/:$PATH
-
-if clipcatctl clear; then
-  notify-send -a "clipcat" "Cleared clipboard history"
-else
-  notify-send -a "clipcat" "Failed to clear clipboard history" --urgency critical
-fi
