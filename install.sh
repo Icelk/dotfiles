0ebf5d35d1b7a50ca83f2c6fbc9091ffe02efcea
Icelk <main@icelk.dev>
2024-07-02 16:38:15 +0200

    Remove obsidian backup service&timer from install

--- install.sh
+++ install.sh
@@ -162,7 +162,7 @@ echo
 
 echo "Enabling user services. Only the hardware-independent are enabled. See this script for the others and the systemd directory."
 
-systemctl --user enable --now xdg-user-dirs-update.service packages-dump.service backup-obsidian.timer check-failed.timer reminder-notify.timer
+systemctl --user enable --now xdg-user-dirs-update.service packages-dump.service check-failed.timer reminder-notify.timer
 # Hardware-dependent
 # systemctl --user enable --now sync.timer backup.timer
 
