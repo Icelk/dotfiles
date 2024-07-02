a20a8e612777d502d7678a31b8bdef7737c7049f
Icelk <main@icelk.dev>
2024-07-02 20:23:51 +0200

    Remove nvidia stuff

--- scripts/wayland.sh
+++ scripts/wayland.sh
@@ -6,11 +6,6 @@ export _JAVA_AWT_WM_NONREPARENTING=1
 export XCURSOR_SIZE=24
 export QT_QPA_PLATFORMTHEME=qt6ct
 
-export LIBVA_DRIVER_NAME=nvidia
-export XDG_SESSION_TYPE=wayland
-export GBM_BACKEND=nvidia-drm
-export __GLX_VENDOR_LIBRARY_NAME=nvidia
-export WLR_NO_HARDWARE_CURSORS=1
 scripts/import-gsettings-themes.sh
 
 exec Hyprland
