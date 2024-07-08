ca96ad12d55cd4f1765d7637a7809f6bbe2935b9
Icelk <main@icelk.dev>
2024-07-08 22:32:35 +0200

    Fix wallpaper & hyprlock

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
+set -euo pipefail
+
+if [ -f /tmp/locked ] ; then exit ; fi
+
+touch /tmp/locked
+(
+    until fprintd-verify | grep "verify-match"; do
+        echo "Failed to verify fingerprint at $(date)" | systemd-cat
+    done
+    echo "Unlocked at $(date)" | systemd-cat
+    pkill -USR1 hyprlock
+) &
 hyprlock
+
+rm /tmp/locked
+kill $(jobs -p)
+pkill fprintd-verify
