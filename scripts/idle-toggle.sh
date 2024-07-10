f8ddba999c176ad06e23f97bb5ae765cba8fbde1
Icelk <main@icelk.dev>
2024-07-10 23:41:34 +0200

    Add hypridle powersave option. Powersave enables by default on each boot

---
+++ scripts/idle-toggle.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+alternate=$(rg -q "# timeout = 30" ~/.config/hypr/hypridle.conf; echo $?)
+
+PATH=~/.cargo/bin:$PATH
+
+if [ $alternate == "0" ]; then
+    corpl -k -e powersave ~/.config/hypr/hypridle.conf
+    notify-send -a "Idle timers" "Reenabled powersave"
+else
+    corpl -k -d powersave ~/.config/hypr/hypridle.conf
+    notify-send -a "Idle timers" "Disabled powersave"
+fi
