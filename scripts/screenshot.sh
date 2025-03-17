14c09598a7ff2bb07dc58da6f7b5bc0706fe9b01
Icelk <main@icelk.dev>
2025-03-17 21:00:55 +0100

    Greatly improve screenshot script

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,7 +2,24 @@
 file=screenshot-"$(date "+%+4Y-%m-%d-%T")".png
 path=~/Pictures/"$file"
 
-grim -g "$(slurp)" $path
+pre_sleep="$1"
+between_sleep="$2"
+
+if [ -n "$pre_sleep" ]; then
+    sleep $pre_sleep
+fi
+
+geometry="$(slurp)"
+status=$?
+if [ $status -gt 0 ]; then
+    exit $status
+fi
+
+if [ -n "$between_sleep" ]; then
+    sleep $between_sleep
+fi
+
+grim -g "$geometry" $path
 
 if [ $? -eq 0 ]; then
     notify-send -a "Screenshot" "Saved screenshot to '~/Pictures/$file'"
