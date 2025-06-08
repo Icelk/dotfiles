00b09e9bbef9a53c50893cf35d84d518b1bfd800
Icelk <main@icelk.dev>
2025-06-08 19:50:08 +0200

    Install batsignal, so I get notifications when battery is running low

--- install.sh
+++ install.sh
@@ -162,7 +162,7 @@ echo
 
 echo "Enabling user services. Only the hardware-independent are enabled. See this script for the others and the systemd directory."
 
-systemctl --user enable --now xdg-user-dirs-update.service packages-dump.service check-failed.timer reminder-notify.timer easyeffects.service
+systemctl --user enable --now xdg-user-dirs-update.service packages-dump.service check-failed.timer reminder-notify.timer easyeffects.service batsignal
 # Hardware-dependent
 # systemctl --user enable --now sync.timer backup.timer
 
