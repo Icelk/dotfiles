3be3bf501270306f3ecee9257ed2b456d6ac5a7b
Icelk <main@icelk.dev>
2026-07-21 14:41:22 +0200

    Minor config changes

--- scripts/boot-windows.sh
+++ scripts/boot-windows.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/sh
 
-windows="$(efibootmgr | grep "Windows")"
+windows="$(efibootmgr | rg "Windows")"
 
 if test -z "$windows"; then
     notify-send -a "Windows® boot" "Failed to boot into Windows; no boot manager was found, or you don't have 'efibootmgr' installed."
     exit 1
 fi
 
-id="$(echo $windows | grep -o "[0-9][0-9][0-9][0-9]" | cut --delimiter \n --fields 1)"
+id="$(echo $windows | rg -o "[0-9][0-9][0-9][0-9]" | head -n1)"
 doas -n efibootmgr -n $id
 if test "$?" != "0"; then
     notify-send -a "Windows® boot" "Failed to set UEFI var NEXT_BOOT. Aborting."
